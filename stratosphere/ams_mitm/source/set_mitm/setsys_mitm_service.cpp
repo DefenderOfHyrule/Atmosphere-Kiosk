df865db22ddc6eb6e49df50f0cee65277f0e53a5
DefenderOfHyrule <mischad@hotmail.nl>
2024-03-06 14:06:28 +0100

    kip time

--- stratosphere/ams_mitm/source/set_mitm/setsys_mitm_service.cpp
+++ stratosphere/ams_mitm/source/set_mitm/setsys_mitm_service.cpp
@@ -269,4 +269,9 @@ namespace ams::mitm::settings {
         R_SUCCEED();
     }
 
+    Result SetSysMitmService::GetQuestFlag(sf::Out<bool> isQuest){
+        isQuest.SetValue(false);
+        R_SUCCEED();
+    }
+
 }
